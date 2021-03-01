ea1d002f6d4cc7718932a6058ef1b06b6c300f91
Jan Safranek <jsafrane@redhat.com>
2021-03-01 15:50:17 +0100

    Add maxUnavailable to DaemonSets
    To speed up cluster upgrade by allowing more nodes to be updated
    simultaneously.

--- pkg/generated/bindata.go
+++ pkg/generated/bindata.go
@@ -258,6 +258,10 @@ spec:
   selector:
     matchLabels:
       app: gcp-pd-csi-driver-node
+  updateStrategy:
+    type: RollingUpdate
+    rollingUpdate:
+      maxUnavailable: 10%
   template:
     metadata:
       labels:
